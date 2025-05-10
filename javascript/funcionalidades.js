63eea1dcd0f2aa4628835de2848b3eec809170d4
Pedro Costa Burilli <105596677+Pcburilli@users.noreply.github.com>
2025-05-10 20:17:47 -0300

    testando inicio de javascript

---
+++ javascript/funcionalidades.js
@@ -0,0 +1,9 @@
+imgs_gangue = document.querySelectorAll(".imgs_gangue")
+
+imgs_gangue.forEach((img) => {
+  img.addEventListener('click', clicar);
+})
+
+function clicar(){
+    alert('CLICOU NA IMAGEM KKKKKKKKKKKKKKK')
+}
